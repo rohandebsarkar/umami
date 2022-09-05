f485e8df8955b47afeaf1f672c2109f24605f434
rohandebsarkar <rohandebsarkar@gmail.com>
2022-09-05 19:53:20 +0530

    fix

--- queries/analytics/session/endSession.js
+++ queries/analytics/session/endSession.js
@@ -17,9 +17,7 @@ async function relationalQuery(session_id) {
         session_id
       },
       data: {
-        update: {
-          ended_at: new Date(),
-        },
+        ended_at: new Date(),
       },
     })
     .then(async res => {
